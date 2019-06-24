90b2cf41ad6704196e6b72fd4f93b1d5beffc30a
wattana.int <wattana.int@ascendcorp.com>
2019-06-24 15:26:01 +0700

    init webpack

--- Dockerfile
+++ Dockerfile
@@ -3,4 +3,8 @@ FROM node:10.16.0-stretch-slim
 ENV PATH $PATH:/node_modules/.bin:/app/
 RUN npm install -g npm@6.9.0
 
+COPY package.json package-lock.json /
+RUN npm install
+
 COPY . /app
+WORKDIR /app
---
+++ app/webpack.config.js
@@ -0,0 +1,44 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    app: './src/index.js',
+    print: './src/print.js'
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: './dist',
+    host: '0.0.0.0'
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      title: 'Development'
+    })
+  ],
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: [
+          'file-loader'
+        ]
+      }
+    ]
+  }
+};
