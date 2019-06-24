d0e0ed049ae6ef25a42b8cc7d2fd2c7a58a8904d
wattana.int <wattana.int@ascendcorp.com>
2019-06-24 15:48:15 +0700

    hot reload for print.js

--- app/src/index.js
+++ app/src/index.js
@@ -1,17 +1,31 @@
 import _ from 'lodash';
 import './style.css';
 import Icon from './icon.png';
+import printMe from './print';
 
 function component() {
   const element = document.createElement('div');
+  const btn = document.createElement('button');
+
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
   element.classList.add('hello');
+
+  btn.innerHTML = 'Click me and check the console!';
+  btn.onclick = printMe;
 
   const myIcon = new Image();
   myIcon.src = Icon;
   element.appendChild(myIcon);
+  element.appendChild(btn);
   
   return element;
 }
 
 document.body.appendChild(component());
+
+if (module.hot) {
+  module.hot.accept('./print', function(){
+    console.log('Accepting the updated printMe module!');
+    printMe();
+  });
+}
--- app/webpack.config.js
+++ app/webpack.config.js
@@ -1,23 +1,27 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const webpack = require('webpack');
 
 module.exports = {
   mode: 'development',
   entry: {
-    app: './src/index.js',
-    print: './src/print.js'
+    app: './src/index.js'
   },
   devtool: 'inline-source-map',
   devServer: {
     contentBase: './dist',
-    host: '0.0.0.0'
+    host: '0.0.0.0',
+    hot: true,
+    sockHost: '0.0.0.0',
+    hotOnly: true
   },
   plugins: [
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
       title: 'Development'
-    })
+    }),
+    new webpack.HotModuleReplacementPlugin()
   ],
   output: {
     filename: '[name].bundle.js',
